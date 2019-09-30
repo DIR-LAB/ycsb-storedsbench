88ef8f045339d023ff3c5b6fa04ad35ee49988af
Dong Dai <daidongly@gmail.com>
2019-09-30 14:56:57 -0400

    update pmem array

--- storeds/array/array_pmem.h
+++ storeds/array/array_pmem.h
@@ -6,15 +6,8 @@
 #define array_pmem_h
 
 int array_pmem_init(const char *path);
-
 int array_pmem_read(const char *key, void *result);
-
-int array_pmem_scan(const char *key, int len, void *result);
-
 int array_pmem_update(const char *key, void *value);
-
 int array_pmem_insert(const char *key, void *value);
-
-int array_pmem_delete(const char *key);
 
 #endif /* array_pmem_h */
