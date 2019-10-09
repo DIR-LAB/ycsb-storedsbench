e039816bbaea4e0e898107623cedffd03d747fdf
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-10-08 21:41:14 -0400

    Update both dram and pmem version of array implementation to store (char *) type data

--- storeds/array/array_dram.c
+++ storeds/array/array_dram.c
@@ -7,9 +7,12 @@
 #include "array_dram.h"
 #include <time.h>
 #include <math.h>
+#include <string.h>
+#include <stdint.h>
 
-int *array = NULL;
+char **array = NULL;
 const int size = 1000000;
+const int default_value_len = 101;
 
 int volatile counter;
 int randIndex;
@@ -23,32 +26,41 @@ int check(){
 }
 
 int array_dram_init(const char *path) {
-    array = (int *) malloc(size * sizeof(int));
+    array = (char **) malloc(size * sizeof(char *));
     if (array == NULL) {
         fprintf(stderr, "malloc failed\n");
         return (-1);
+    }
+    for(int i=0; i<size; i+=1) {
+        array[i] = (char *) malloc(default_value_len * sizeof(char *));
     }
     return 1;
 }
 
 int array_dram_read(const char *key, void *result) {
     check();
-    result = &array[atol(key) % size];
+    uint64_t uint64_key = strtoull(key, NULL, 0);
+    int offset = (int) (uint64_key % size);
+    result = &array[offset];
     return 1;
 }
 
 int array_dram_update(const char *key, void *value) {
     check();
-    array[atol(key) % size] = atoi((const char *) value);
+    uint64_t uint64_key = strtoull(key, NULL, 0);
+    int offset = (int) (uint64_key % size);
+    strcpy(array[offset], (const char *) value);
     return 1;
 }
 
 int array_dram_insert(const char *key, void *value) {
     check();
-    array[atol(key) % size] = atoi((const char *) value);
+    uint64_t uint64_key = strtoull(key, NULL, 0);
+    int offset = (int) (uint64_key % size);
+    strcpy(array[offset], (const char *) value);
     return 1;
 }
 
 void array_dram_free() {
-    //todo: implement later
+    free(array);
 }
--- storeds/array/array_pmem.c
+++ storeds/array/array_pmem.c
@@ -20,13 +20,19 @@
 #define PM_ARRAY_POOL_SIZE    (160 * 1024 * 1024)
 
 POBJ_LAYOUT_BEGIN(array);
-POBJ_LAYOUT_TOID(array, int);
+POBJ_LAYOUT_TOID(array, struct array_elm);
 POBJ_LAYOUT_TOID(array, PMEMoid);
+POBJ_LAYOUT_TOID(array, TOID(struct array_elm));
 POBJ_LAYOUT_TOID(array, struct array_info);
 POBJ_LAYOUT_END(array);
 
 static PMEMobjpool *pop;
 static TOID(struct array_info) array_info;
+static TOID(struct array_elm) array_elm;
+
+struct array_elm {
+	char *value;
+};
 
 struct array_info {
     PMEMoid array;
@@ -34,6 +40,7 @@ struct array_info {
 
 struct array_info *info = NULL;
 const int pmem_array_size = 1000000;
+const int default_value_len = 101;
 
 int pmem_array_check(){
     if ((info->array).off == 0) {
@@ -41,6 +48,57 @@ int pmem_array_check(){
         return (-1);
     }
     return 1;
+}
+
+/*
+ * print_toid -- print array of TOID(struct array_elm) type
+ */
+void print_toid(struct array_info *info) {
+	TOID_ARRAY(TOID(struct array_elm)) array;
+	TOID_ASSIGN(array, info->array);
+	for (int i = 0; i < pmem_array_size; i++) {
+        printf("array_elm = %s\n", D_RO(D_RO(array)[i])->value);
+    }
+}
+
+/*
+ * elm_constructor -- constructor of array_elm type object
+ */
+int elm_constructor(PMEMobjpool *pop, void *ptr, void *arg) {
+	struct array_elm *obj = (struct array_elm *)ptr;
+    obj->value = (char *) malloc(default_value_len * sizeof(char *));
+	pmemobj_persist(pop, obj, sizeof(*obj));
+
+	return 0;
+}
+
+/*
+ * alloc_toid -- allocate array of TOID(struct array_elm) type
+ */
+PMEMoid alloc_toid() {
+	TOID_ARRAY(TOID(struct array_elm)) array;
+	/*
+	 * To allocate persistent array of TOID with user-defined structure type
+	 * is necessary to allocate pointer with size equal to number of
+	 * elements multiplied by size of TOID of proper type and to allocate
+	 * each of elements separately.
+	 */
+	POBJ_ALLOC(pop, &array, TOID(struct array_elm), sizeof(TOID(struct array_elm)) * pmem_array_size, NULL, NULL);
+
+	if (TOID_IS_NULL(array)) {
+		fprintf(stderr, "POBJ_ALLOC\n");
+		return OID_NULL;
+	}
+
+	for (size_t i = 0; i < pmem_array_size; i++) {
+        char init_string[10] = "amar ami";
+		POBJ_NEW(pop, &D_RW(array)[i], struct array_elm, elm_constructor, &init_string);
+		if (TOID_IS_NULL(D_RW(array)[i])) {
+			fprintf(stderr, "POBJ_ALLOC\n");
+			assert(0);
+		}
+	}
+	return array.oid;
 }
 
 int array_pmem_init(const char *path){
@@ -59,52 +117,76 @@ int array_pmem_init(const char *path){
 
     POBJ_ZNEW(pop, &array_info, struct array_info);
     info = D_RW(array_info);
-    POBJ_ALLOC(pop, &info->array, int, sizeof(int) * pmem_array_size, NULL, NULL);
+    info->array = alloc_toid();
     if ((info->array).off == 0) {
         fprintf(stderr, "POBJ_ALLOC\n");
         return -1;
     }
-    pmemobj_persist(pop, info, pmem_array_size * sizeof(*info));
+    pmemobj_persist(pop, info, sizeof(*info));
     return 1;
 }
 
 int array_pmem_read(const char *key, void *result){
     pmem_array_check();
-    TOID(int) array;
+    TOID_ARRAY(TOID(struct array_elm)) array;
     TOID_ASSIGN(array, info->array);
-    int counter = D_RO(array)[atol(key) % pmem_array_size];
+
+    uint64_t uint64_key = strtoull(key, NULL, 0);
+    int offset = (int) (uint64_key % pmem_array_size);
+
+    char *counter = D_RO(D_RO(array)[offset])->value;
     result = &counter;
+
     return 1;
 }
 
 int array_pmem_update(const char *key, void *value){
     pmem_array_check();
-    TOID(int) array;
+    TOID_ARRAY(TOID(struct array_elm)) array;
     TOID_ASSIGN(array, info->array);
-    int offset = atol(key) % pmem_array_size;
-    //D_RW(array)[offset] = atoi((const char *) value);
-    D_RW(array)[offset] = 5;
+
+    uint64_t uint64_key = strtoull(key, NULL, 0);
+    int offset = (int) (uint64_key % pmem_array_size);
+
+    strcpy(D_RW(D_RW(array)[offset])->value, (const char *) value);
     pmemobj_persist(pop, &D_RW(array)[offset], sizeof(int));
+
     return 1;
 }
 
 int array_pmem_insert(const char *key, void *value){
-    printf("insert: %s %s\n", key, (const char *) value);
     pmem_array_check();
-    TOID(int) array;
+    TOID_ARRAY(TOID(struct array_elm)) array;
     TOID_ASSIGN(array, info->array);
+
     uint64_t uint64_key = strtoull(key, NULL, 0);
     int offset = (int) (uint64_key % pmem_array_size);
-    //D_RW(array)[offset] = atoi((const char *) value);
-    D_RW(array)[offset] = 5;
+
+    strcpy(D_RW(D_RW(array)[offset])->value, (const char *) value);
     pmemobj_persist(pop, &D_RW(array)[offset], sizeof(int));
+
     return 1;
 }
 
-void array_pmem_free() {
-    TOID(int) array;
+/*
+ * free_toid -- de-allocate array of TOID(struct array_elm) type
+ */
+void free_toid() {
+    TOID_ARRAY(TOID(struct array_elm)) array;
     TOID_ASSIGN(array, info->array);
+    /*
+	 * When there is persistent array of persistent pointer type allocated,
+	 * there is necessary to de-allocate each element, if they were
+	 * allocated earlier
+	 */
+	for (int i = 0; i < pmem_array_size; i++) {
+        POBJ_FREE(&D_RW(array)[i]);
+    }
     POBJ_FREE(&array);
+}
+
+void array_pmem_free() {
+    free_toid();
     POBJ_FREE(&array_info);
     pmemobj_close(pop);
 }
