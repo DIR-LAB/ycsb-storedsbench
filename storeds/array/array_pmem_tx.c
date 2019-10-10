6ebf735320b245e1020a8511db5a2659d9245694
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-10-10 16:01:00 -0400

    Broken commit for transactional version of array_pmem

--- storeds/array/array_pmem_tx.c
+++ storeds/array/array_pmem_tx.c
@@ -19,39 +19,51 @@
 #define MAX_TYPE_NUM 8
 #define PM_ARRAY_POOL_SIZE    (160 * 1024 * 1024)
 
-POBJ_LAYOUT_BEGIN(array);
-POBJ_LAYOUT_TOID(array, struct array_elm);
-POBJ_LAYOUT_TOID(array, PMEMoid);
-POBJ_LAYOUT_TOID(array, TOID(struct array_elm));
-POBJ_LAYOUT_TOID(array, struct array_info);
-POBJ_LAYOUT_END(array);
+POBJ_LAYOUT_BEGIN(array_tx);
+POBJ_LAYOUT_ROOT(array_tx, struct array_root);
+POBJ_LAYOUT_TOID(array_tx, struct array_elm);
+POBJ_LAYOUT_TOID(array_tx, PMEMoid);
+POBJ_LAYOUT_TOID(array_tx, TOID(struct array_elm));
+POBJ_LAYOUT_TOID(array_tx, struct array_info);
+POBJ_LAYOUT_END(array_tx);
 
 static PMEMobjpool *pop;
 static TOID(struct array_info) array_info;
 static TOID(struct array_elm) array_elm;
+const TOID(struct array_root) root;
+
+//struct array_info *info_tx = NULL;
+const int pmem_array_size = 1000000;
+const int default_value_len = 101;
+
+//todo: start from here
+struct array_root {
+    TOID(struct array_info) info;
+};
+
 
 struct array_elm {
-	char *value;
+	char value[default_value_len];
+    //char *value;
 };
 
 struct array_info {
     PMEMoid array;
 };
 
-struct array_info *info_tx = NULL;
-const int pmem_array_size = 1000000;
-const int default_value_len = 101;
-
 int pmem_tx_array_check(){
-    if ((info_tx->array).off == 0) {
+    const struct array_root *root_p = D_RO(root);
+    const struct array_info *info = D_RO(root_p->info);
+
+    if ((info->array).off == 0) {
         fprintf(stderr, "array not initialized yet\n");
-        return (-1);
+        assert(0);
     }
     return 1;
 }
 
 /*
- * print_toid -- print array of TOID(struct array_elm) type
+ * print_toid_tx -- print array of TOID(struct array_elm) type
  */
 void print_toid_tx(struct array_info *info_tx) {
 	TOID_ARRAY(TOID(struct array_elm)) array;
@@ -62,18 +74,20 @@ void print_toid_tx(struct array_info *info_tx) {
 }
 
 /*
- * elm_constructor -- constructor of array_elm type object
+ * elm_constructor_tx -- constructor of array_elm type object
  */
-int elm_constructor_tx(PMEMobjpool *pop, void *ptr, void *arg) {
+/*int elm_constructor_tx(PMEMobjpool *pop, void *ptr, void *arg) {
+    //note: how the following block can be put into a tx block
 	struct array_elm *obj = (struct array_elm *)ptr;
     obj->value = (char *) malloc(default_value_len * sizeof(char *));
-	pmemobj_persist(pop, obj, sizeof(*obj));
+    //note: we will not need this manual persist
+	//pmemobj_persist(pop, obj, sizeof(*obj));
 
 	return 0;
-}
+}*/
 
 /*
- * alloc_toid -- allocate array of TOID(struct array_elm) type
+ * alloc_toid_tx -- allocate array of TOID(struct array_elm) type
  */
 PMEMoid alloc_toid_tx() {
 	TOID_ARRAY(TOID(struct array_elm)) array;
@@ -83,21 +97,28 @@ PMEMoid alloc_toid_tx() {
 	 * elements multiplied by size of TOID of proper type and to allocate
 	 * each of elements separately.
 	 */
-	POBJ_ALLOC(pop, &array, TOID(struct array_elm), sizeof(TOID(struct array_elm)) * pmem_array_size, NULL, NULL);
+    TX_BEGIN(pop) {
+        TX_ADD(array);
+        array = TX_ALLOC(TOID(struct array_elm), sizeof(TOID(struct array_elm)) * pmem_array_size);
+	    //POBJ_ALLOC(pop, &array, TOID(struct array_elm), sizeof(TOID(struct array_elm)) * pmem_array_size, NULL, NULL);
 
-	if (TOID_IS_NULL(array)) {
-		fprintf(stderr, "POBJ_ALLOC\n");
-		return OID_NULL;
-	}
+        if (TOID_IS_NULL(array)) {
+            fprintf(stderr, "POBJ_ALLOC\n");
+            return OID_NULL;
+        }
 
-	for (size_t i = 0; i < pmem_array_size; i++) {
-        char init_string[10] = "amar ami";
-		POBJ_NEW(pop, &D_RW(array)[i], struct array_elm, elm_constructor_tx, &init_string);
-		if (TOID_IS_NULL(D_RW(array)[i])) {
-			fprintf(stderr, "POBJ_ALLOC\n");
-			assert(0);
-		}
-	}
+        /*for (size_t i = 0; i < pmem_array_size; i++) {
+            printf("amar ami nai re amar ami nai ...\n");
+            //note: how we can use TX_ALLOC instead of the following line?
+            POBJ_NEW(pop, &D_RW(array)[i], struct array_elm, elm_constructor_tx, NULL);
+            //D_RW(array)[i] = TX_ALLOC(struct array_elm, default_value_len * sizeof(char *));
+            printf("%s\n", D_RO(D_RO(array)[i])->value);
+            if (TOID_IS_NULL(D_RO(array)[i])) {
+                fprintf(stderr, "POBJ_ALLOC\n");
+                assert(0);
+            }
+        }*/
+    } TX_END;
 	return array.oid;
 }
 
@@ -115,26 +136,103 @@ int array_pmem_tx_init(const char *path){
         }
     }
 
-    POBJ_ZNEW(pop, &array_info, struct array_info);
-    info_tx = D_RW(array_info);
-    info_tx->array = alloc_toid_tx();
-    if ((info_tx->array).off == 0) {
-        fprintf(stderr, "POBJ_ALLOC\n");
-        return -1;
-    }
-    pmemobj_persist(pop, info_tx, sizeof(*info_tx));
+    TOID(struct array_root) root = POBJ_ROOT(pop, struct array_root);
+    struct array_root *root_p = D_RW(root);
+    struct array_info *info_tx = D_RW(root_p->info);
+
+    TX_BEGIN(pop) {
+        printf("into transaction\n");
+        //todo: need to bring root here
+        TX_ADD(root);
+        //note: not sure what should be added in the transaction here
+        printf("added root to transaction\n");
+        //POBJ_ZNEW(pop, &array_info, struct array_info);
+        //TOID(struct array_info) ar_info = TX_ZNEW(struct array_info);
+        //struct array_root *root_p = D_RW(root);
+        //TX_ADD(root_p->info);
+        printf("added info to transaction\n");
+        //struct array_info *info_tx = D_RW(root_p->info);
+        info_tx = D_RW(TX_ZNEW(struct array_info));
+        printf("going to allocate other stuffs\n");
+        
+        //info_tx->array = alloc_toid_tx();
+        //info_tx->array = TX_ALLOC(TOID(struct array_elm), sizeof(TOID(struct array_elm)) * pmem_array_size).oid;
+        POBJ_ALLOC(pop, &info_tx->array, TOID(struct array_elm), sizeof(TOID(struct array_elm)) * pmem_array_size, NULL, NULL);
+    // } TX_END;
+        
+    // printf("array_elm size: %ld\n", sizeof(TOID(struct array_elm)));
+    // printf("info_tx->array size: %ld\n", sizeof(info_tx->array));
+    // printf("allocation done\n");
+    
+    // TX_BEGIN(pop) {
+
+        TOID_ARRAY(TOID(struct array_elm)) array;
+        POBJ_ALLOC(pop, &array, TOID(struct array_elm), sizeof(TOID(struct array_elm)) * pmem_array_size, NULL, NULL);
+
+        printf("info_tx->array size: %ld\n", sizeof(info_tx->array));
+        printf("allocated size: %ld\n", sizeof(TOID(struct array_elm)) * pmem_array_size);
+        printf("size: %ld\n", sizeof(array));
+	    if (TOID_IS_NULL(array)) {
+		    fprintf(stderr, "POBJ_ALLOC\n");
+		    abort();
+	    }
+        for (size_t i = 0; i < pmem_array_size; i++) {
+            printf("->i=%ld\n", i);
+            
+            //TOID_ASSIGN(array, info_tx->array);
+            D_RW(array)[i] = TX_ZNEW(struct array_elm);
+		    //POBJ_NEW(pop, &D_RW(info_tx->array)[i], struct array_elm, elm_constructor, NULL);
+		    if (TOID_IS_NULL(D_RW(array)[i])) {
+			    fprintf(stderr, "POBJ_ALLOC\n");
+                //abort();
+			    assert(0);
+		    }
+	    }
+
+        printf("array after loop: %ld\n", sizeof(array));
+        info_tx->array = array.oid;
+
+        if(info_tx == NULL) printf("info_tx is null\n");
+        else printf("info_tx is not null\n");
+        printf("%ld\n", (info_tx->array).off);
+        
+        //info_tx->array = TX_ALLOC(TOID(struct array_elm), sizeof(TOID(struct array_elm)) * pmem_array_size).oid;
+        /*if ((info_tx->array).off == 0) {
+            fprintf(stderr, "POBJ_ALLOC\n");
+            abort();
+            //note: need rollback
+        }*/
+        printf("here I come ...\n");
+        /*for (size_t i = 0; i < pmem_array_size; i++) {
+            POBJ_NEW(pop, &D_RW(info_tx->array)[i], struct array_elm, elm_constructor_tx, NULL);
+            //D_RW(info_tx->array)[i] = TX_ALLOC(struct array_elm, default_value_len * sizeof(char *));
+            printf("%s\n", D_RO(D_RO(array)[i])->value);
+            if (TOID_IS_NULL(D_RO(array)[i])) {
+                fprintf(stderr, "POBJ_ALLOC\n");
+                assert(0);
+            }
+        }*/
+
+        //pmem_tx_array_check();
+        //note: we will not need this manual persist
+        //pmemobj_persist(pop, info_tx, sizeof(*info_tx));
+    } TX_END;
+    printf("transaction done\n");
     return 1;
 }
 
 int array_pmem_tx_read(const char *key, void *result){
     pmem_tx_array_check();
+    const struct array_root *root_p = D_RO(root);
+    const struct array_info *info = D_RO(root_p->info);
     TOID_ARRAY(TOID(struct array_elm)) array;
-    TOID_ASSIGN(array, info_tx->array);
+    TOID_ASSIGN(array, info->array);
 
     uint64_t uint64_key = strtoull(key, NULL, 0);
     int offset = (int) (uint64_key % pmem_array_size);
 
-    char *counter = D_RO(D_RO(array)[offset])->value;
+    //note: we don't need transaction support for read operation. is that correct?
+    const char *counter = D_RO(D_RO(array)[offset])->value;
     result = &counter;
 
     return 1;
@@ -142,51 +240,74 @@ int array_pmem_tx_read(const char *key, void *result){
 
 int array_pmem_tx_update(const char *key, void *value){
     pmem_tx_array_check();
+    const struct array_root *root_p = D_RO(root);
+    const struct array_info *info = D_RO(root_p->info);
     TOID_ARRAY(TOID(struct array_elm)) array;
-    TOID_ASSIGN(array, info_tx->array);
+    TOID_ASSIGN(array, info->array);
 
     uint64_t uint64_key = strtoull(key, NULL, 0);
     int offset = (int) (uint64_key % pmem_array_size);
 
-    strcpy(D_RW(D_RW(array)[offset])->value, (const char *) value);
-    pmemobj_persist(pop, &D_RW(array)[offset], sizeof(int));
+    TX_BEGIN(pop) {
+        TX_ADD(array);
+        strcpy(D_RW(D_RW(array)[offset])->value, (const char *) value);
+    } TX_END;
 
     return 1;
 }
 
 int array_pmem_tx_insert(const char *key, void *value){
     pmem_tx_array_check();
+    printf("Insert: %s %s\n", key, (char *)value);
+    const struct array_root *root_p = D_RO(root);
+    const struct array_info *info = D_RO(root_p->info);
     TOID_ARRAY(TOID(struct array_elm)) array;
-    TOID_ASSIGN(array, info_tx->array);
+    TOID_ASSIGN(array, info->array);
 
     uint64_t uint64_key = strtoull(key, NULL, 0);
     int offset = (int) (uint64_key % pmem_array_size);
 
-    strcpy(D_RW(D_RW(array)[offset])->value, (const char *) value);
-    pmemobj_persist(pop, &D_RW(array)[offset], sizeof(int));
+    TX_BEGIN(pop) {
+        TX_ADD(array);
+        //printf("-> %s\n", D_RO(D_RO(array)[offset])->value);
+        strcpy(D_RW(D_RW(array)[offset])->value, (const char *) value);
+    } TX_END;
 
     return 1;
 }
 
 /*
- * free_toid -- de-allocate array of TOID(struct array_elm) type
+ * free_toid_tx -- de-allocate array of TOID(struct array_elm) type
  */
 void free_toid_tx() {
+    const struct array_root *root_p = D_RO(root);
+    const struct array_info *info = D_RO(root_p->info);
     TOID_ARRAY(TOID(struct array_elm)) array;
-    TOID_ASSIGN(array, info_tx->array);
+    TOID_ASSIGN(array, info->array);
     /*
 	 * When there is persistent array of persistent pointer type allocated,
 	 * there is necessary to de-allocate each element, if they were
 	 * allocated earlier
 	 */
-	for (int i = 0; i < pmem_array_size; i++) {
-        POBJ_FREE(&D_RW(array)[i]);
-    }
-    POBJ_FREE(&array);
+    TX_BEGIN(pop) {
+        TX_ADD(array);
+        for (int i = 0; i < pmem_array_size; i++) {
+            POBJ_FREE(&D_RW(array)[i]);
+        }
+        POBJ_FREE(&array);
+    } TX_END;
 }
 
 void array_pmem_tx_free() {
-    free_toid_tx();
-    POBJ_FREE(&array_info);
+    TX_BEGIN(pop) {
+        TX_ADD(root);
+        free_toid_tx();
+        const struct array_root *root_p = D_RO(root);
+        const struct array_info *info = D_RO(root_p->info);
+        POBJ_FREE(info);
+        //note: now info_tx.array is freed, how to make it null?
+        //refference: https://pmem.io/2015/06/17/tx-alloc.html
+        POBJ_FREE(root_p);     //note: how to free info_tx here?
+    } TX_END;
     pmemobj_close(pop);
 }
