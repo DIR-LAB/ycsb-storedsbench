0c61a5ef8d984f938a1ca2ec986d3549065dd6a4
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-10-14 21:47:55 -0400

    Added method implementation signatures in linkedlist_dram

--- storeds/linkedlist/linkedlist_dram.c
+++ storeds/linkedlist/linkedlist_dram.c
@@ -3,3 +3,23 @@
 //
 
 #include "linkedlist_dram.h"
+
+int linkedlist_dram_init(const char *path) {
+    return 1;
+}
+
+int linkedlist_dram_read(const char *key, void *result) {
+    return 1;
+}
+
+int linkedlist_dram_update(const char *key, void *value) {
+    return 1;
+}
+
+int linkedlist_dram_insert(const char *key, void *value) {
+    return 1;
+}
+
+void linkedlist_dram_free() {
+    //
+}
