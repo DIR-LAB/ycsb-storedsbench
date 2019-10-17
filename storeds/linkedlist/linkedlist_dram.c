0897783021d244afd31349baa9083278de491f35
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-10-17 13:15:30 -0400

    Fixed issues in linkedlist_dram

--- storeds/linkedlist/linkedlist_dram.c
+++ storeds/linkedlist/linkedlist_dram.c
@@ -4,101 +4,103 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <math.h>
 #include <string.h>
 #include <stdint.h>
+#include <assert.h>
 
 #include "linkedlist_dram.h"
 
 const int size = 1000000;
 const int default_value_len = 101;
 
-
-typedef struct Node{
-    uint64_t newKey;
-    char newValue[default_value_len];
-    struct Node *next;
+struct node {
+    uint64_t key;
+    char value[default_value_len];
+    struct node *next;
 };
 
-struct Node *head = NULL;
-struct Node *tail = NULL;
+static struct node *head = NULL;
+static struct node *tail = NULL;
 
-
-
-int check () {
+int linkedlist_dram_check() {
     if (head == NULL) {
-        fprintf(stderr, "malloc failed\n");
-        return (-1);
+        fprintf(stderr, "[%s]: FATAL: linkedlist not initialized yet\n", __FUNCTION__);
+        assert(0);
     }
     return 1;
 }
 
 // Check if this is the correct implementation for the init function for a linkedlist
 int linkedlist_dram_init(const char *path) {
-    if (head == NULL) {
-        fprintf(stderr, "malloc failed\n");
-    }
-
-    for (int i = 0; i < size; i++) {
-        *head->newValue = (char *) malloc(default_value_len * sizeof(char *));
-    }
+    head = NULL;
+    tail = NULL;
     return 1;
 }
 
 int linkedlist_dram_read(const char *key, void *result) {
-    check();
-    struct Node *current_node = head;
-    
-    while(current_node != Ngi tULL) {
-        result = &current_node->next;
-    }
+    //printf("[%s]: key: %s\n", __FUNCTION__, key);
+    linkedlist_dram_check();
 
+    uint64_t uint64_key = strtoull(key, NULL, 0);
+    struct node *current_node = head;
 
-    return 1;
-}
-
-int linkedlist_dram_update(const char *key, void *value) {
-    check();
-    struct Node* current_node = head;
-
-    current_node = head;
-    while(current_node->next != NULL) {
-        if(current_node->newKey = key) {
-            strcpy(current_node->newKey, (const char *) value); 
+    while (current_node != NULL) {
+        if (current_node->key == uint64_key) {
+            result = current_node->value;
+            break;
         }
-
         current_node = current_node->next;
     }
 
     return 1;
 }
 
-int linkedlist_dram_insert(const char *key, void *value) {
-    check();
-    struct Node* node = (struct Node*) malloc(sizeof(node));
-    node->newKey = key;
-    strcpy(node->newKey,(const char *) value);
+int linkedlist_dram_update(const char *key, void *value) {
+    //printf("[%s]: key: %s, value: %s\n", __FUNCTION__, key, (char *) value);
+    linkedlist_dram_check();
 
-    if (head == NULL) {
-       tail = node;
-       head = tail;
+    uint64_t uint64_key = strtoull(key, NULL, 0);
+    struct node *current_node = head;
+
+    while (current_node != NULL) {
+        if (current_node->key == uint64_key) {
+            strcpy(current_node->value, (const char *) value);
+            break;
+        }
+        current_node = current_node->next;
     }
+    return 1;
+}
 
-    while (tail->next != NULL) {
+int linkedlist_dram_insert(const char *key, void *value) {
+    //printf("[%s]: key: %s, value: %s\n", __FUNCTION__, key, (char *) value);
+    //linkedlist_dram_check();
+
+    uint64_t uint64_key = strtoull(key, NULL, 0);
+    struct node *new_node = (struct node *) malloc(sizeof(struct node));
+    new_node->key = uint64_key;
+    strcpy(new_node->value, (const char *) value);
+
+    if(head == NULL) {
+        head = new_node;
+        tail = new_node;
+    }
+    else {
+        tail->next = new_node;
         tail = tail->next;
-    } 
-    tail->next = node;
+    }
 
     return 1;
 }
 
 void linkedlist_dram_free() {
-    struct Node *current_node = head;
+    struct node *current_node;
 
     while (head != NULL) {
         current_node = head;
         head = head->next;
         free(current_node);
     }
+    free(head);
+    free(tail);
 }
