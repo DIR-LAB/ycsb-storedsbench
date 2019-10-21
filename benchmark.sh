87ad30033569f06cb8a076ce87df4e79a125bd0a
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-10-21 19:27:20 -0400

    Add linkedlist in benchmark checking bash script

--- benchmark.sh
+++ benchmark.sh
@@ -33,16 +33,6 @@ echo "[Benchmark] array-pmem-tx"
 ./ycsbc -db storeds -threads 1 -dbpath /pmem/array.pmem -type array-pmem-tx -P workloads/workloadf.spec
 echo "*****************<>*****************"
 
-#linkedlist-dram
-#echo "[Benchmark] linkedlist-dram"
-#./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type linkedlist-dram -P workloads/workloada.spec
-#./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type linkedlist-dram -P workloads/workloadb.spec
-#./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type linkedlist-dram -P workloads/workloadc.spec
-#./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type linkedlist-dram -P workloads/workloadd.spec
-#./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type linkedlist-dram -P workloads/workloade.spec
-#./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type linkedlist-dram -P workloads/workloadf.spec
-#echo "*****************<>*****************"
-
 #hashtable-dram
 echo "[Benchmark] hashtable-dram"
 ./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type hashtable-dram -P workloads/workloada.spec
@@ -54,14 +44,14 @@ echo "[Benchmark] hashtable-dram"
 echo "*****************<>*****************"
 
 #hashtable-pmem
-#echo "[Benchmark] hashtable-pmem"
-#./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type hashtable-pmem -P workloads/workloada.spec
-#./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type hashtable-pmem -P workloads/workloadb.spec
-#./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type hashtable-pmem -P workloads/workloadc.spec
-#./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type hashtable-pmem -P workloads/workloadd.spec
-#./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type hashtable-pmem -P workloads/workloade.spec
-#./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type hashtable-pmem -P workloads/workloadf.spec
-#echo "*****************<>*****************"
+echo "[Benchmark] hashtable-pmem"
+./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type hashtable-pmem -P workloads/workloada.spec
+./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type hashtable-pmem -P workloads/workloadb.spec
+./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type hashtable-pmem -P workloads/workloadc.spec
+./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type hashtable-pmem -P workloads/workloadd.spec
+# ./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type hashtable-pmem -P workloads/workloade.spec
+./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type hashtable-pmem -P workloads/workloadf.spec
+echo "*****************<>*****************"
 
 #hashtable-pmem-tx
 echo "[Benchmark] hashtable-pmem-tx"
@@ -71,4 +61,34 @@ echo "[Benchmark] hashtable-pmem-tx"
 ./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type hashtable-pmem-tx -P workloads/workloadd.spec
 #./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type hashtable-pmem-tx -P workloads/workloade.spec
 ./ycsbc -db storeds -threads 1 -dbpath /pmem/hashtable.pmem -type hashtable-pmem-tx -P workloads/workloadf.spec
+echo "*****************<>*****************"
+
+#linkedlist-dram
+echo "[Benchmark] linkedlist-dram"
+./ycsbc -db storeds -threads 1 -dbpath /pmem/linkedlist.pmem -type linkedlist-dram -P workloads/workloada.spec
+./ycsbc -db storeds -threads 1 -dbpath /pmem/linkedlist.pmem -type linkedlist-dram -P workloads/workloadb.spec
+./ycsbc -db storeds -threads 1 -dbpath /pmem/linkedlist.pmem -type linkedlist-dram -P workloads/workloadc.spec
+./ycsbc -db storeds -threads 1 -dbpath /pmem/linkedlist.pmem -type linkedlist-dram -P workloads/workloadd.spec
+# ./ycsbc -db storeds -threads 1 -dbpath /pmem/linkedlist.pmem -type linkedlist-dram -P workloads/workloade.spec
+./ycsbc -db storeds -threads 1 -dbpath /pmem/linkedlist.pmem -type linkedlist-dram -P workloads/workloadf.spec
+echo "*****************<>*****************"
+
+#linkedlist-pmem
+echo "[Benchmark] linkedlist-pmem"
+./ycsbc -db storeds -threads 1 -dbpath /pmem/linkedlist.pmem -type linkedlist-pmem -P workloads/workloada.spec
+./ycsbc -db storeds -threads 1 -dbpath /pmem/linkedlist.pmem -type linkedlist-pmem -P workloads/workloadb.spec
+./ycsbc -db storeds -threads 1 -dbpath /pmem/linkedlist.pmem -type linkedlist-pmem -P workloads/workloadc.spec
+./ycsbc -db storeds -threads 1 -dbpath /pmem/linkedlist.pmem -type linkedlist-pmem -P workloads/workloadd.spec
+# ./ycsbc -db storeds -threads 1 -dbpath /pmem/linkedlist.pmem -type linkedlist-pmem -P workloads/workloade.spec
+./ycsbc -db storeds -threads 1 -dbpath /pmem/linkedlist.pmem -type linkedlist-pmem -P workloads/workloadf.spec
+echo "*****************<>*****************"
+
+#linkedlist-pmem
+echo "[Benchmark] linkedlist-pmem-tx"
+./ycsbc -db storeds -threads 1 -dbpath /pmem/linkedlist.pmem -type linkedlist-pmem-tx -P workloads/workloada.spec
+./ycsbc -db storeds -threads 1 -dbpath /pmem/linkedlist.pmem -type linkedlist-pmem-tx -P workloads/workloadb.spec
+./ycsbc -db storeds -threads 1 -dbpath /pmem/linkedlist.pmem -type linkedlist-pmem-tx -P workloads/workloadc.spec
+./ycsbc -db storeds -threads 1 -dbpath /pmem/linkedlist.pmem -type linkedlist-pmem-tx -P workloads/workloadd.spec
+# ./ycsbc -db storeds -threads 1 -dbpath /pmem/linkedlist.pmem -type linkedlist-pmem-tx -P workloads/workloade.spec
+./ycsbc -db storeds -threads 1 -dbpath /pmem/linkedlist.pmem -type linkedlist-pmem-tx -P workloads/workloadf.spec
 echo "*****************<>*****************"
