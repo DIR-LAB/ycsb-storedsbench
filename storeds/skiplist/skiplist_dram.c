e2d85c3dbe28a06437021ed398c8557521642e57
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-10-24 16:01:41 -0400

    Broken commit for skiplist
    Benchmark performance is too low, currently it is 4.40, need to check the read procedure again. Also there is an issue of "double free or corruption" while freeing memory, need to fix that as well.

--- storeds/skiplist/skiplist_dram.c
+++ storeds/skiplist/skiplist_dram.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <assert.h>
 
 #include "../ex_common.h"
@@ -39,59 +40,135 @@ struct sk_node {
 
 static struct sk_node *head = NULL;
 
-int skiplist_dram_init(const char *path) {
-    head = (struct sk_node *) malloc(sizeof(struct sk_node));
+/*
+ * skiplist_dram_check -- (internal) checks if skiplist is initialized or not
+ */
+int skiplist_dram_check() {
+	if (head == NULL) {
+        fprintf(stderr, "[%s]: FATAL: skiplist not initialized yet\n", __FUNCTION__);
+        assert(0);
+    }
     return 1;
 }
 
+/**
+ * skiplist_dram_init -- initialize skiplist
+ */
+int skiplist_dram_init(const char *path) {
+    srand((uint32_t) time(NULL));
+    head = (struct sk_node *) malloc(sizeof(struct sk_node));
+    for(int i=0; i<SKIPLIST_LEVELS_NUM; i+=1) {
+        head->next[i] = NULL;
+    }
+    skiplist_dram_check();
+    return 1;
+}
+
+/**
+ * skiplist_dram_find -- (internal) returns path to searched node
+ * or if node doesn't exist, it will return path to place where key should be.
+ */
 void skiplist_dram_find(uint64_t uint64_key, struct sk_node **path) {
-    struct sk_node *current_node = head;
+    //printf("[%s] called in\n", __func__);
+    struct sk_node *active_node = head;
+    //if(head == NULL) printf("head is null ...\n");
     for(int current_level = SKIPLIST_LEVELS_NUM - 1; current_level >= 0; current_level -= 1) {
-        current_node = current_node->next[current_level];
+        //printf("current_level: %d\n", current_level);
+        struct sk_node *current_node = active_node->next[current_level];
+        //if(current_node == NULL) printf("current_node is null ...\n");
+        //printf("before the while loop ...\n");
         while(current_node != NULL && current_node->entry.key < uint64_key) {
-            current_node = current_node->next[current_level];
+            //printf("into the while loop\n");
+            active_node = current_node;
+            current_node = active_node->next[current_level];
         }
-        //printf("current_level[%d] => node <%lu, %s>\n", current_level, current_node->entry.key, current_node->entry.value);
-        path[current_level] = current_node;
+        //if(active_node != NULL) printf("current_level[%d] => node <%lu, %s>\n", current_level, active_node->entry.key, active_node->entry.value);
+        path[current_level] = active_node;
     }
 }
 
+/**
+ * skiplist_dram_read -- read value of key and sets into result
+ */
 int skiplist_dram_read(const char *key, void *result) {
+    skiplist_dram_check();
+    printf("[%s]: PARAM: key: %s\n", __func__, key);
     //todo: add checker method
 
     uint64_t uint64_key = strtoull(key, NULL, 0);
-    struct sk_node *path[SKIPLIST_LEVELS_NUM];
+    struct sk_node *path[SKIPLIST_LEVELS_NUM], *possible_found;
     skiplist_dram_find(uint64_key, path);
-    if(path[SKIPLIST_BASE_LEVEL]->next[SKIPLIST_BASE_LEVEL] != NULL && path[SKIPLIST_BASE_LEVEL]->next[SKIPLIST_BASE_LEVEL]->entry.key == uint64_key) {
-        result = path[SKIPLIST_BASE_LEVEL]->next[SKIPLIST_BASE_LEVEL]->entry.value;
+    possible_found = path[SKIPLIST_BASE_LEVEL]->next[SKIPLIST_BASE_LEVEL];
+    if(possible_found != NULL && possible_found->entry.key == uint64_key) {
+        result = possible_found->entry.value;
     }
     return 1;
 }
 
+/**
+ * skiplist_dram_update -- if key exist, update the value part in skiplist
+ * if key doesn't exist, insert new key-value pair into skiplist
+ */
 int skiplist_dram_update(const char *key, void *value) {
+    skiplist_dram_check();
+    printf("[%s]: PARAM: key: %s, value: %s\n", __func__, key, (char *) value);
     skiplist_dram_insert(key, value);
     return 1;
 }
 
+/**
+ * skiplist_dram_insert_node -- (internal) insert new node in selected places
+ */
+void skiplist_dram_insert_node(struct sk_node *new_node, struct sk_node *path[SKIPLIST_LEVELS_NUM]) {
+    int current_level = SKIPLIST_BASE_LEVEL;
+
+    // certainly assign node to level 0, and randomly for other levels
+    do {
+        new_node->next[current_level] = path[current_level]->next[current_level];
+        path[current_level]->next[current_level] = new_node;
+    } while(++current_level < SKIPLIST_LEVELS_NUM && !(rand() % 2));
+}
+
+/**
+ * skiplist_dram_insert -- insert new key-value pair into skiplist
+ * if key already exist, update the value part in skiplist
+ */
 int skiplist_dram_insert(const char *key, void *value) {
+    skiplist_dram_check();
+    printf("[%s]: PARAM: key: %s, value: %s\n", __func__, key, (char *) value);
     uint64_t uint64_key = strtoull(key, NULL, 0);
-    struct sk_node *path[SKIPLIST_LEVELS_NUM];
+    struct sk_node *path[SKIPLIST_LEVELS_NUM], *possible_found;
 
     skiplist_dram_find(uint64_key, path);
+    possible_found = path[SKIPLIST_BASE_LEVEL]->next[SKIPLIST_BASE_LEVEL];
 
-    if(path[SKIPLIST_BASE_LEVEL]->next[SKIPLIST_BASE_LEVEL] != NULL && path[SKIPLIST_BASE_LEVEL]->next[SKIPLIST_BASE_LEVEL]->entry.key == uint64_key) {
+    if(possible_found != NULL && possible_found->entry.key == uint64_key) {
         //key already exist, update value
-        strcpy(path[SKIPLIST_BASE_LEVEL]->next[SKIPLIST_BASE_LEVEL]->entry.value, (char *) value);
-    } else {
+        strcpy(possible_found->entry.value, (char *) value);
+    }
+    else {
         //key not exist, insert the new node
-
         struct sk_node *new_node = (struct sk_node *) malloc(sizeof(struct sk_node));
         new_node->entry.key = uint64_key;
         strcpy(new_node->entry.value, (char *) value);
+        skiplist_dram_insert_node(new_node, path);
     }
     return 1;
 }
 
+/**
+ * skiplist_dram_free -- dealocate memory of skiplist
+ */
 void skiplist_dram_free() {
-    //
+    skiplist_dram_check();
+    for(int i=0; i<SKIPLIST_LEVELS_NUM; i+=1) {
+        struct sk_node *current = head->next[i];
+        while(current != NULL) {
+            struct sk_node *tmp = current;
+            current = current->next[i];
+            free(tmp);
+        }
+        free(head->next[i]);
+    }
+    free(head);
 }
