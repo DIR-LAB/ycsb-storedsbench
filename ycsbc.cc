06adcb412da4f760808769b22c421c284cbbdb19
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-10-31 10:40:40 -0400

    Update ycsbc log print

--- ycsbc.cc
+++ ycsbc.cc
@@ -70,7 +70,7 @@ int main(const int argc, const char *argv[]) {
         assert(n.valid());
         sum += n.get();
     }
-    cerr << "# Loading records:\t" << sum << endl;
+    cout << "# Loading records:\t" << sum << endl;
 
     // Peforms transactions
     actual_ops.clear();
@@ -89,9 +89,9 @@ int main(const int argc, const char *argv[]) {
         sum += n.get();
     }
     double duration = timer.End();
-    cerr << "# Transaction throughput (KTPS)" << endl;
-    cerr << props["dbname"] << '\t' << file_name << '\t' << num_threads << '\t';
-    cerr << total_ops / duration / 1000 << endl;
+    cout << "# Transaction throughput (KTPS)" << endl;
+    cout << props["dbname"] << '\t' << file_name << '\t' << num_threads << '\t';
+    cout << total_ops / duration / 1000 << endl;
     if (db) db->~DB();
 }
 
