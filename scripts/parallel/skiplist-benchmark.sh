7c257c628b6290b29adfcc25d54c54bb02793174
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-11-14 18:44:44 -0500

    Added parallel benchmark results for skiplist

---
+++ scripts/parallel/skiplist-benchmark.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#cd ../../
+#make clean
+#make
+#cd scripts/parallel/
+
+input_path="../../workloads/"
+
+#skiplist-dram
+for file in $input_path*.spec; do
+  n_threads=1
+  while [ $n_threads -le 32 ]
+  do
+    counter=1
+    echo "[Benchmark] skiplist-dram, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 10 ]
+    do
+      ./../../ycsbc_parallel -db storeds -threads $n_threads -dbpath /pmem/skiplist -type skiplist-dram -P $input_path${file##*/}
+      ((counter++))
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
+
+#skiplist-pmem
+for file in $input_path*.spec; do
+  n_threads=1
+  while [ $n_threads -le 32 ]
+  do
+    counter=1
+    echo "[Benchmark] skiplist-pmem, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 10 ]
+    do
+      ./../../ycsbc_parallel -db storeds -threads $n_threads -dbpath /pmem/skiplist -type skiplist-pmem -P $input_path${file##*/}
+      ((counter++))
+      rm /pmem/*.pmem
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
+
+#skiplist-pmem-tx
+for file in $input_path*.spec; do
+  n_threads=1
+  while [ $n_threads -le 32 ]
+  do
+    counter=1
+    echo "[Benchmark] skiplist-pmem-tx, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 10 ]
+    do
+      ./../../ycsbc_parallel -db storeds -threads $n_threads -dbpath /pmem/skiplist -type skiplist-pmem-tx -P $input_path${file##*/}
+      ((counter++))
+      rm /pmem/*.pmem
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
