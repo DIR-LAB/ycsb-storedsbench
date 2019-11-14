7dcb9cd353ecad4eab7471999eebcccf9404e3fb
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-11-13 22:16:23 -0500

    Add btree parallel benchmark script

---
+++ scripts/parallel/btree-benchmark.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#cd ../../
+#make clean
+#make
+#cd scripts/parallel/
+
+input_path="../../workloads/"
+
+#btree-dram
+for file in $input_path*.spec; do
+  n_threads=1
+  while [ $n_threads -le 32 ]
+  do
+    counter=1
+    echo "[Benchmark] btree-dram, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 10 ]
+    do
+      ./../../ycsbc_parallel -db storeds -threads $n_threads -dbpath /pmem/btree -type btree-dram -P $input_path${file##*/}
+      ((counter++))
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
+
+#btree-pmem
+for file in $input_path*.spec; do
+  n_threads=1
+  while [ $n_threads -le 32 ]
+  do
+    counter=1
+    echo "[Benchmark] btree-pmem, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 10 ]
+    do
+      ./../../ycsbc_parallel -db storeds -threads $n_threads -dbpath /pmem/btree -type btree-pmem -P $input_path${file##*/}
+      ((counter++))
+      rm /pmem/*.pmem
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
+
+#btree-pmem-tx
+for file in $input_path*.spec; do
+  n_threads=1
+  while [ $n_threads -le 32 ]
+  do
+    counter=1
+    echo "[Benchmark] btree-pmem-tx, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 10 ]
+    do
+      ./../../ycsbc_parallel -db storeds -threads $n_threads -dbpath /pmem/btree -type btree-pmem-tx -P $input_path${file##*/}
+      ((counter++))
+      rm /pmem/*.pmem
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
