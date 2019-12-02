1c2e43ef60ddd009ccaa564e9e6e2f23691fd072
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-12-02 00:51:31 -0500

    Add bash scripts to retrieve parallel and concurrent rbtree benchmark results

---
+++ scripts/concurrent/rbtree-benchmark.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#cd ../../
+#make clean
+#make
+#cd scripts/parallel/
+
+input_path="../../workloads/"
+
+#rbtree-dram
+for file in $input_path*.spec; do
+  n_threads=1
+  while [ $n_threads -le 32 ]
+  do
+    counter=1
+    echo "[Benchmark] rbtree-dram, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 1 ]
+    do
+      ./../../ycsbc -db storeds -threads $n_threads -dbpath /pmem/rbtree.pmem -type rbtree-dram-conc-lock -P $input_path${file##*/}
+      ((counter++))
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
+
+#rbtree-pmem
+for file in $input_path*.spec; do
+  n_threads=1
+  while [ $n_threads -le 32 ]
+  do
+    counter=1
+    echo "[Benchmark] rbtree-pmem, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 1 ]
+    do
+      ./../../ycsbc -db storeds -threads $n_threads -dbpath /pmem/rbtree.pmem -type rbtree-pmem-conc-lock -P $input_path${file##*/}
+      ((counter++))
+      rm /pmem/*.pmem
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
+
+#rbtree-pmem-tx
+for file in $input_path*.spec; do
+  n_threads=1
+  while [ $n_threads -le 32 ]
+  do
+    counter=1
+    echo "[Benchmark] rbtree-pmem-tx, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 1 ]
+    do
+      ./../../ycsbc -db storeds -threads $n_threads -dbpath /pmem/rbtree.pmem -type rbtree-pmem-tx-conc-lock -P $input_path${file##*/}
+      ((counter++))
+      rm /pmem/*.pmem
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
---
+++ scripts/parallel/rbtree-benchmark.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#cd ../../
+#make clean
+#make
+#cd scripts/parallel/
+
+input_path="../../workloads/"
+
+#rbtree-dram
+for file in $input_path*.spec; do
+  n_threads=1
+  while [ $n_threads -le 32 ]
+  do
+    counter=1
+    echo "[Benchmark] rbtree-dram, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 1 ]
+    do
+      ./../../ycsbc_parallel -db storeds -threads $n_threads -dbpath /pmem/rbtree -type rbtree-dram -P $input_path${file##*/}
+      ((counter++))
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
+
+#rbtree-pmem
+for file in $input_path*.spec; do
+  n_threads=1
+  while [ $n_threads -le 32 ]
+  do
+    counter=1
+    echo "[Benchmark] rbtree-pmem, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 1 ]
+    do
+      ./../../ycsbc_parallel -db storeds -threads $n_threads -dbpath /pmem/rbtree -type rbtree-pmem -P $input_path${file##*/}
+      ((counter++))
+      rm /pmem/*.pmem
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
+
+#rbtree-pmem-tx
+for file in $input_path*.spec; do
+  n_threads=1
+  while [ $n_threads -le 32 ]
+  do
+    counter=1
+    echo "[Benchmark] rbtree-pmem-tx, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 1 ]
+    do
+      ./../../ycsbc_parallel -db storeds -threads $n_threads -dbpath /pmem/rbtree -type rbtree-pmem-tx -P $input_path${file##*/}
+      ((counter++))
+      rm /pmem/*.pmem
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
