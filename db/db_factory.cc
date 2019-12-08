005e4a24919c7d19602a52fdef0b1139c9f0fc58
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-12-08 17:37:57 -0500

    Update db_factory, remove unnecessary databases from the list

--- db/db_factory.cc
+++ db/db_factory.cc
@@ -9,10 +9,6 @@
 #include "db_factory.h"
 
 #include <string>
-#include "basic_db.h"
-#include "lock_stl_db.h"
-#include "tbb_rand_db.h"
-#include "tbb_scan_db.h"
 #include "storeds_db.h"
 
 using namespace std;
@@ -20,19 +16,7 @@ using ycsbc::DB;
 using ycsbc::DBFactory;
 
 DB *DBFactory::CreateDB(utils::Properties &props) {
-    if (props["dbname"] == "basic") {
-        return new BasicDB;
-    } else if (props["dbname"] == "lock_stl") {
-        return new LockStlDB;
-    } /*else if (props["dbname"] == "redis") {
-        int port = stoi(props["port"]);
-        int slaves = stoi(props["slaves"]);
-        return new RedisDB(props["host"].c_str(), port, slaves);
-    }*/ else if (props["dbname"] == "tbb_rand") {
-        return new TbbRandDB;
-    } else if (props["dbname"] == "tbb_scan") {
-        return new TbbScanDB;
-    } else if (props["dbname"] == "storeds") {
+    if (props["dbname"] == "storeds") {
         return new StoredsDB(props.GetProperty("type", "array-dram").c_str(),
                 props.GetProperty("dbpath", "/pmem/array.pmem").c_str());
     } else {
