6cef90202fcf63b702b61f4b9b8b0b7e19a72ee2
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-12-31 00:55:11 -0500

    Add basic files for concurrent vmem data-structures

---
+++ storeds/hashmap/ht_vmem_concurrent_mlock.cc
@@ -0,0 +1,246 @@
+//
+// Created by Islam, Abdullah Al Raqibul on 10/15/19.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdint.h>
+#include <string.h>
+#include <libvmem.h>
+#include "ht_common.h"
+
+namespace ycsbc {
+    class HtVmemConcurrentMLock : public StoredsBase {
+    public:
+        HtVmemConcurrentMLock(const char *path) {
+            HtVmemConcurrentMLock::init(path);
+        }
+
+        int init(const char *path);
+
+        int read(const uint64_t key, void *&result);
+
+        int scan(const uint64_t key, int len, std::vector <std::vector<DB::Kuint64VstrPair>> &result);
+
+        int update(const uint64_t key, void *value);
+
+        int insert(const uint64_t key, void *value);
+
+        void destroy();
+
+        ~HtVmemConcurrentMLock();
+
+    private:
+        /* Private Data */
+        VMEM *vmp;
+        struct dram_hashtable *root_p;
+
+        int check();
+
+        void print();
+
+        uint64_t hash_function(uint64_t value);
+
+        int reinit(size_t new_len);
+
+        struct dram_entry* new_entry(uint64_t key, const char* value);
+
+        void free_entry_chain(struct dram_entry *head);
+    };
+
+    /*
+     * check -- checks if hashtable has been initialized
+     */
+    inline int HtVmemConcurrentMLock::check() {
+        if (root_p == NULL || root_p->buckets == NULL) {
+            fprintf(stderr, "[%s]: FATAL: hashtable not initialized yet\n", __FUNCTION__);
+            assert(0);
+        }
+        return 1;
+    }
+
+    /*
+    * print -- prints complete hashtable state
+    */
+    void HtVmemConcurrentMLock::print() {
+        srand((uint32_t) time(NULL));
+        check();
+
+        struct dram_buckets *buckets_p = root_p->buckets;
+
+        printf("a: %d, b: %d, p: %lu\n", root_p->hash_fun_coeff_a, root_p->hash_fun_coeff_b, root_p->hash_fun_coeff_p);
+        printf("total buckets: %lu\n", buckets_p->nbuckets);
+
+        for (size_t i = 0; i < buckets_p->nbuckets; ++i) {
+            if (buckets_p->bucket[i] == NULL) continue;
+
+            int num = 0;
+            printf("bucket #%lu:", i);
+            for (struct dram_entry *entry_p = buckets_p->bucket[i]; entry_p != NULL; entry_p = entry_p->next) {
+                printf(" <%lu, %s>", entry_p->key, entry_p->value);
+                num++;
+            }
+            printf(" (%d)\n", num);
+        }
+    }
+
+    /*
+     * hash_function_dram -- the simplest hashing function,
+     * see https://en.wikipedia.org/wiki/Universal_hashing#Hashing_integers
+     */
+    uint64_t HtVmemConcurrentMLock::hash_function(uint64_t value) {
+        uint32_t a = root_p->hash_fun_coeff_a;
+        uint32_t b = root_p->hash_fun_coeff_b;
+        uint64_t p = root_p->hash_fun_coeff_p;
+        size_t len = root_p->buckets->nbuckets;
+
+        return ((value * a + b) % p) % len;
+    }
+
+    /*
+     * reinit -- reinitialize the hashtable with a new number of buckets, not implemented yet
+     */
+    int HtVmemConcurrentMLock::reinit(size_t new_len) {
+        return 1;
+    }
+
+    /*
+     * init -- hashtable initializer
+     */
+    int HtVmemConcurrentMLock::init(const char *path) {
+        if ((vmp = vmem_create(path, PMEM_BTREE_POOL_SIZE)) == NULL) {
+            fprintf(stderr, "[%s]: FATAL: vmem_create failed\n", __FUNCTION__);
+            exit(1);
+        }
+
+        size_t len = INIT_BUCKETS_NUM;
+        if ((root_p = ((struct dram_hashtable *) vmem_malloc(vmp, sizeof(struct dram_hashtable)))) == NULL) {
+            fprintf(stderr, "[%s]: FATAL: vmem_malloc failed\n", __FUNCTION__);
+            exit(1);
+        }
+        root_p->seed = (uint32_t)time(NULL);
+        do {
+            root_p->hash_fun_coeff_a = (uint32_t) rand();
+        } while(root_p->hash_fun_coeff_a == 0);
+
+        root_p->hash_fun_coeff_b = (uint32_t) rand();
+        root_p->hash_fun_coeff_p = HASH_FUNC_COEFF_P;
+        if ((root_p->buckets = ((struct dram_buckets *) vmem_malloc(vmp, sizeof(struct dram_buckets)))) == NULL) {
+            fprintf(stderr, "[%s]: FATAL: vmem_malloc failed\n", __FUNCTION__);
+            exit(1);
+        }
+        root_p->buckets->nbuckets = len;
+        if ((root_p->buckets->bucket = ((struct dram_entry **) vmem_calloc(vmp, len, sizeof(struct dram_entry)))) == NULL) {
+            fprintf(stderr, "[%s]: FATAL: vmem_malloc failed\n", __FUNCTION__);
+            exit(1);
+        }
+
+        return 1;
+    }
+
+    int HtVmemConcurrentMLock::scan(const uint64_t key, int len, std::vector <std::vector<DB::Kuint64VstrPair>> &result) {
+        throw "Scan: function not implemented!";
+    }
+
+    /**
+     * read -- read 'value' of 'key' and place it into '&result'
+     */
+    int HtVmemConcurrentMLock::read(const uint64_t key, void *&result) {
+        check();
+
+        struct dram_buckets *buckets_p = root_p->buckets;
+        //uint64_t uint64_key = strtoull(key, NULL, 0);
+        uint64_t hash_value = hash_function(key);
+
+        for(struct dram_entry *entry_p = buckets_p->bucket[hash_value]; entry_p != NULL; entry_p = entry_p->next) {
+            if(entry_p->key == key) {
+                //key found! put it to result and return
+                result = entry_p->value;
+                break;
+            }
+        }
+        return 1;
+    }
+
+    /**
+     * update -- update 'value' of 'key' into the hashtable, will insert the 'value' if 'key' not exists
+     */
+    int HtVmemConcurrentMLock::update(const uint64_t key, void *value) {
+        return insert(key, value);
+    }
+
+    /**
+     * new_entry -- create new hashtable entry
+     */
+    struct dram_entry* HtVmemConcurrentMLock::new_entry(uint64_t key, const char* value) {
+        struct dram_entry *entry_p;
+        if ((entry_p = ((struct dram_entry *) vmem_malloc(vmp, sizeof(struct dram_entry)))) == NULL) {
+            fprintf(stderr, "[%s]: FATAL: vmem_malloc failed\n", __FUNCTION__);
+            exit(1);
+        }
+        entry_p->key = key;
+        memcpy(entry_p->value, (char *) value, strlen((char *) value) + 1);
+        if ((entry_p->next = ((struct dram_entry *) vmem_malloc(vmp, sizeof(struct dram_entry)))) == NULL) {
+            fprintf(stderr, "[%s]: FATAL: vmem_malloc failed\n", __FUNCTION__);
+            exit(1);
+        }
+        return entry_p;
+    }
+
+    /**
+     * insert -- inserts 'value' into the hashtable, will update the 'value' if 'key' already exists
+     */
+    int HtVmemConcurrentMLock::insert(const uint64_t key, void *value) {
+        check();
+
+        struct dram_buckets *buckets_p = root_p->buckets;
+        //uint64_t uint64_key = strtoull(key, NULL, 0);
+        uint64_t hash_value = hash_function(key);
+
+        for(struct dram_entry *entry_p = buckets_p->bucket[hash_value]; entry_p != NULL; entry_p = entry_p->next) {
+            if(entry_p->key == key) {
+                //key found! replace the value and return
+                memcpy(entry_p->value, (char *) value, strlen((char *) value) + 1);
+                return 1;
+            }
+        }
+
+        //key not found! need to insert data into bucket[hash_value]
+        struct dram_entry *entry_p = new_entry(key, (const char*) value);
+        entry_p->next = buckets_p->bucket[hash_value];
+        buckets_p->bucket[hash_value] = entry_p;
+        return 1;
+    }
+
+    /**
+     * free_entry_chain -- free memory allocated in a hashtable bucket entry chain
+     */
+    void HtVmemConcurrentMLock::free_entry_chain(struct dram_entry *head) {
+        struct dram_entry* entry_p;
+        while(head != NULL) {
+            entry_p = head;
+            head = head->next;
+            vmem_free(vmp, entry_p);
+        }
+    }
+
+    /**
+     * free -- destructor
+     */
+    void HtVmemConcurrentMLock::destroy() {
+        check();
+        for (int i=0; i < INIT_BUCKETS_NUM; i+=1) {
+            struct dram_entry *entry_p = root_p->buckets->bucket[i];
+            if (entry_p != NULL) {
+                free_entry_chain(entry_p);
+            }
+        }
+        vmem_free(vmp, root_p->buckets->bucket);
+        vmem_free(vmp, root_p->buckets);
+        vmem_free(vmp, root_p);
+        vmem_delete(vmp);
+    }
+}   //ycsbc
---
+++ storeds/skiplist/skiplist_vmem_concurrent_mlock.cc
@@ -0,0 +1,253 @@
+//
+// Created by aislam6 on 10/16/19.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <assert.h>
+
+#include "skiplist_common.h"
+
+namespace ycsbc {
+    class SkiplistVmemConcurrentMLock : public StoredsBase {
+    public:
+        SkiplistVmemConcurrentMLock(const char *path) {
+            SkiplistVmemConcurrentMLock::init(path);
+        }
+
+        int init(const char *path);
+
+        int read(const uint64_t key, void *&result);
+
+        int scan(const uint64_t key, int len, std::vector <std::vector<DB::Kuint64VstrPair>> &result);
+
+        int update(const uint64_t key, void *value);
+
+        int insert(const uint64_t key, void *value);
+
+        void destroy();
+
+        ~SkiplistVmemConcurrentMLock();
+
+    private:
+        /* Private Data */
+        VMEM *vmp;
+        struct sk_dram_node *head = NULL;
+
+        int check();
+
+        void print();
+
+        void find(uint64_t uint64_key, struct sk_dram_node **path);
+
+        void insert_node(struct sk_dram_node *new_node, struct sk_dram_node *path[SKIPLIST_LEVELS_NUM]);
+
+        void remove_node(struct sk_dram_node *path[SKIPLIST_LEVELS_NUM]);
+
+        void remove_free(uint64_t key);
+    };
+
+    /*
+     * check -- (internal) checks if skiplist is initialized or not
+     */
+    int SkiplistVmemConcurrentMLock::check() {
+        if (head == NULL) {
+            fprintf(stderr, "[%s]: FATAL: skiplist not initialized yet\n", __FUNCTION__);
+            assert(0);
+        }
+        return 1;
+    }
+
+    /**
+     * init -- initialize skiplist
+     */
+    int SkiplistVmemConcurrentMLock::init(const char *path) {
+        srand((uint32_t) time(NULL));
+        if ((vmp = vmem_create(path, PMEM_BTREE_POOL_SIZE)) == NULL) {
+            fprintf(stderr, "[%s]: FATAL: vmem_create failed\n", __FUNCTION__);
+            exit(1);
+        }
+
+        if ((head = ((struct sk_dram_node *) vmem_malloc(vmp, sizeof(struct sk_dram_node)))) == NULL) {
+            fprintf(stderr, "[%s]: FATAL: vmem_malloc failed\n", __FUNCTION__);
+            exit(1);
+        }
+
+        for(int i=0; i<SKIPLIST_LEVELS_NUM; i+=1) {
+            head->next[i] = NULL;
+        }
+        check();
+        return 1;
+    }
+
+    /**
+     * print -- (internal) level-wise print all the <key-value> pairs stored in skiplist
+     */
+    void SkiplistVmemConcurrentMLock::print() {
+        int current_level = SKIPLIST_LEVELS_NUM - 1;
+
+        while(current_level >= SKIPLIST_BASE_LEVEL) {
+            struct sk_dram_node *current_node = head->next[current_level];
+            printf("level[%d]", current_level);
+            while(current_node != NULL) {
+                printf(" <%lu, %s> ->", current_node->entry.key, current_node->entry.value);
+                current_node = current_node->next[current_level];
+            }
+            printf("<END>\n");
+            current_level -= 1;
+        }
+    }
+
+    int SkiplistVmemConcurrentMLock::scan(const uint64_t key, int len, std::vector <std::vector<DB::Kuint64VstrPair>> &result) {
+        //printf("[%s]: PARAM: key: %s\n", __func__, key);
+        check();
+
+        struct sk_dram_node *path[SKIPLIST_LEVELS_NUM], *possible_found;
+        find(key, path);
+        possible_found = path[SKIPLIST_BASE_LEVEL]->next[SKIPLIST_BASE_LEVEL];
+        int len_count = 0;
+        while(possible_found != NULL) {
+            std::vector <DB::Kuint64VstrPair> tmp;
+            tmp.push_back(std::make_pair(possible_found->entry.key, possible_found->entry.value));
+            result.push_back(tmp);
+
+            len_count += 1;
+            if(len_count == len) break;
+            possible_found = possible_found->next[SKIPLIST_BASE_LEVEL];
+        }
+        return 1;
+    }
+
+    /**
+     * find -- (internal) returns path to searched node
+     * or if node doesn't exist, it will return path to place where key should be.
+     */
+    void SkiplistVmemConcurrentMLock::find(uint64_t uint64_key, struct sk_dram_node **path) {
+        //printf("[%s] called in\n", __func__);
+        struct sk_dram_node *active_node = head;
+        for(int current_level = SKIPLIST_LEVELS_NUM - 1; current_level >= 0; current_level -= 1) {
+            struct sk_dram_node *current_node = active_node->next[current_level];
+            while(current_node != NULL && current_node->entry.key < uint64_key) {
+                active_node = current_node;
+                current_node = active_node->next[current_level];
+            }
+            path[current_level] = active_node;
+        }
+    }
+
+    /**
+     * read -- read value of key and sets into result
+     */
+    int SkiplistVmemConcurrentMLock::read(const uint64_t key, void *&result) {
+        //printf("[%s]: PARAM: key: %s\n", __func__, key);
+        check();
+
+        //uint64_t uint64_key = strtoull(key, NULL, 0);
+        struct sk_dram_node *path[SKIPLIST_LEVELS_NUM], *possible_found;
+        find(key, path);
+        possible_found = path[SKIPLIST_BASE_LEVEL]->next[SKIPLIST_BASE_LEVEL];
+        if(possible_found != NULL && possible_found->entry.key == key) {
+            result = possible_found->entry.value;
+        }
+        return 1;
+    }
+
+    /**
+     * update -- if key exist, update the value part in skiplist
+     * if key doesn't exist, insert new key-value pair into skiplist
+     */
+    int SkiplistVmemConcurrentMLock::update(const uint64_t key, void *value) {
+        //printf("[%s]: PARAM: key: %s, value: %s\n\n", __func__, key, (char *) value);
+        insert(key, value);
+        return 1;
+    }
+
+    /**
+     * insert_node -- (internal) insert new node in selected places
+     */
+    void SkiplistVmemConcurrentMLock::insert_node(struct sk_dram_node *new_node, struct sk_dram_node *path[SKIPLIST_LEVELS_NUM]) {
+        int current_level = SKIPLIST_BASE_LEVEL;
+
+        // certainly assign node to level 0, and randomly for other levels
+        do {
+            new_node->next[current_level] = path[current_level]->next[current_level];
+            path[current_level]->next[current_level] = new_node;
+        } while(++current_level < SKIPLIST_LEVELS_NUM && !(rand() % 2));
+    }
+
+    /**
+     * insert -- insert new key-value pair into skiplist
+     * if key already exist, update the value part in skiplist
+     */
+    int SkiplistVmemConcurrentMLock::insert(const uint64_t key, void *value) {
+        //printf("[%s]: PARAM: key: %ld, value: %s\n\n", __func__, key, (char *) value);
+        check();
+        //uint64_t uint64_key = strtoull(key, NULL, 0);
+        struct sk_dram_node *path[SKIPLIST_LEVELS_NUM], *possible_found;
+
+        find(key, path);
+        possible_found = path[SKIPLIST_BASE_LEVEL]->next[SKIPLIST_BASE_LEVEL];
+
+        if(possible_found != NULL && possible_found->entry.key == key) {
+            //key already exist, update value
+            strcpy(possible_found->entry.value, (char *) value);
+        }
+        else {
+            //key not exist, insert the new node
+            struct sk_dram_node *new_node;
+            if ((new_node = ((struct sk_dram_node *) vmem_malloc(vmp, sizeof(struct sk_dram_node)))) == NULL) {
+                fprintf(stderr, "[%s]: FATAL: vmem_malloc failed\n", __FUNCTION__);
+                exit(1);
+            }
+            new_node->entry.key = key;
+            strcpy(new_node->entry.value, (char *) value);
+            insert_node(new_node, path);
+        }
+        return 1;
+    }
+
+    /*
+     * remove_node -- (internal) removes selected node
+     */
+    void SkiplistVmemConcurrentMLock::remove_node(struct sk_dram_node *path[SKIPLIST_LEVELS_NUM]) {
+        struct sk_dram_node *possible_remove = path[SKIPLIST_BASE_LEVEL]->next[SKIPLIST_BASE_LEVEL];
+        for(int i=SKIPLIST_BASE_LEVEL; i<SKIPLIST_LEVELS_NUM; i+=1) {
+            if(path[i]->next[i] != NULL && (path[i]->next[i]->entry.key == possible_remove->entry.key)) {
+                //struct sk_dram_node *tmp = path[i]->next[i];
+                path[i]->next[i] = possible_remove->next[i];
+                //free(tmp);
+            }
+        }
+        vmem_free(vmp, possible_remove);
+        //todo: need careful analysis if this memory de-allocation is the right way or not!
+    }
+
+    /*
+     * remove_free -- (internal) removes key-value pair from the list
+     */
+    void SkiplistVmemConcurrentMLock::remove_free(uint64_t key) {
+        struct sk_dram_node *path[SKIPLIST_LEVELS_NUM], *possible_remove;
+        find(key, path);
+        possible_remove = path[SKIPLIST_BASE_LEVEL]->next[SKIPLIST_BASE_LEVEL];
+        if(possible_remove != NULL && possible_remove->entry.key == key) {
+            remove_node(path);
+        }
+    }
+
+    /**
+     * destroy -- deallocate memory of skiplist
+     */
+    void SkiplistVmemConcurrentMLock::destroy() {
+        check();
+
+        while(head->next[0] != NULL) {
+            struct sk_dram_node *next = head->next[0];
+            remove_free(next->entry.key);
+        }
+        vmem_free(vmp, head);
+        head = NULL;
+        vmem_delete(vmp);
+    }
+}   //ycsbc
