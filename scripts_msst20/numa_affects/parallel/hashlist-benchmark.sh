bad2ca58796cae55047ce461c2950bad2514469b
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2020-02-19 17:05:43 -0500

    Add benchmark script for hashlist

---
+++ scripts_msst20/numa_affects/parallel/hashlist-benchmark.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#cd ../../
+#make clean
+#make
+#cd scripts/parallel/
+
+input_path="../../../workloads/"
+
+#hashlist-dram
+for file in $input_path*e.spec; do
+  n_threads=1
+  while [ $n_threads -le 1 ]
+  do
+    counter=1
+    echo "[Benchmark] hashlist-dram, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 10 ]
+    do
+      ./../../../ycsbc_parallel_non_numa -db storeds -threads $n_threads -dbpath /pmem/hl -type hl-dram -P $input_path${file##*/}
+      ((counter++))
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
+
+#hashlist-vmem
+for file in $input_path*e.spec; do
+  n_threads=1
+  while [ $n_threads -le 1 ]
+  do
+    counter=1
+    echo "[Benchmark] hashlist-vmem, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 10 ]
+    do
+      ./../../../ycsbc_parallel_non_numa -db storeds -threads $n_threads -dbpath /pmem -type hl-vmem -P $input_path${file##*/}
+      ((counter++))
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
+
+#hashlist-pmem
+for file in $input_path*e.spec; do
+  n_threads=1
+  while [ $n_threads -le 1 ]
+  do
+    counter=1
+    echo "[Benchmark] hashlist-pmem, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 10 ]
+    do
+      ./../../../ycsbc_parallel_non_numa -db storeds -threads $n_threads -dbpath /pmem/hl -type hl-pmem -P $input_path${file##*/}
+      ((counter++))
+      rm /pmem/hl*.pmem
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
+
+#hashlist-pmem-tx
+for file in $input_path*e.spec; do
+  n_threads=1
+  while [ $n_threads -le 1 ]
+  do
+    counter=1
+    echo "[Benchmark] hashlist-pmem-tx, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 10 ]
+    do
+      ./../../../ycsbc_parallel_non_numa -db storeds -threads $n_threads -dbpath /pmem/hl -type hl-pmem-tx -P $input_path${file##*/}
+      ((counter++))
+      rm /pmem/hl*.pmem
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
