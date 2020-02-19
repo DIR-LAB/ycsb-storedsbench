1f0dfc47a387faba6d70d895fa8a061c33853ef1
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2020-02-18 22:13:11 -0500

    Add script to measure the performance of btree with embedded value

---
+++ scripts_msst20/device_characteristics/parallel/btree-benchmark.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#cd ../../
+#make clean
+#make
+#cd scripts/parallel/
+
+input_path="../../../workloads/"
+
+#btree-dram
+for file in $input_path*a.spec; do
+  n_threads=1
+  while [ $n_threads -le 1 ]
+  do
+    counter=1
+    echo "[Benchmark] btree-dram, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 10 ]
+    do
+      ./../../../ycsbc_parallel -db storeds -threads $n_threads -dbpath /pmem/btree -type btree-dram -P $input_path${file##*/}
+      ((counter++))
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
+
+#btree-vmem
+for file in $input_path*a.spec; do
+  n_threads=1
+  while [ $n_threads -le 1 ]
+  do
+    counter=1
+    echo "[Benchmark] btree-vmem, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 10 ]
+    do
+      ./../../../ycsbc_parallel -db storeds -threads $n_threads -dbpath /pmem -type btree-vmem -P $input_path${file##*/}
+      ((counter++))
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
+
+#btree-pmem
+for file in $input_path*a.spec; do
+  n_threads=1
+  while [ $n_threads -le 1 ]
+  do
+    counter=1
+    echo "[Benchmark] btree-pmem, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 10 ]
+    do
+      ./../../../ycsbc_parallel -db storeds -threads $n_threads -dbpath /pmem/btree -type btree-pmem -P $input_path${file##*/}
+      ((counter++))
+      rm /pmem/btree*.pmem
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
+
+#btree-pmem-tx
+for file in $input_path*a.spec; do
+  n_threads=1
+  while [ $n_threads -le 1 ]
+  do
+    counter=1
+    echo "[Benchmark] btree-pmem-tx, #of_threads: " $n_threads ", workload: ${file##*/}"
+    while [ $counter -le 10 ]
+    do
+      ./../../../ycsbc_parallel -db storeds -threads $n_threads -dbpath /pmem/btree -type btree-pmem-tx -P $input_path${file##*/}
+      ((counter++))
+      rm /pmem/btree*.pmem
+    done
+    echo "*****************<>*****************"
+    ((n_threads*=2))
+  done
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~<>~~~~~~~~~~~~~~~~~~~~~~~~~"
+done
