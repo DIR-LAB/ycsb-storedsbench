7616febd4acbdbb58814dc46b3aff240dbd96e2b
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2020-02-19 12:25:46 -0500

    Add null check before deleting linkedlist tail pointer

--- storeds/linkedlist/linkedlist_pmem_concurrent_mlock.cc
+++ storeds/linkedlist/linkedlist_pmem_concurrent_mlock.cc
@@ -150,7 +150,7 @@ namespace ycsbc {
             root_p->head = ((struct ll_pmem_node *) pmemobj_direct(root_p->head))->next;
             pmemobj_free(&current_node);
         }
-        pmemobj_free(&(root_p->tail));
+        if(root_p->tail.off != 0) pmemobj_free(&(root_p->tail));
         root_p->head = OID_NULL;
         root_p->tail = OID_NULL;
         root_oid = OID_NULL;
